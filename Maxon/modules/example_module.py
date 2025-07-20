b6fffc27f6ca678db366e3cdc8edaec37ee2bec3
nerdkux <90501615+nerkux@users.noreply.github.com>
2025-07-20 14:37:05 +1000

    module structrure updated

--- Maxon/modules/example_module.py
+++ Maxon/modules/example_module.py
@@ -5,7 +5,7 @@ __description__ = "module_description"
 __version__ = "1.0"
 __author__ = "@username"
 
-async def handle(packet, args):
+async def handle(packet, args, client):
     return "example answer"
 
 def register(handlers):
