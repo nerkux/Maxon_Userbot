fb531d8728c95623bde46ee3c100fa9c04b25a3e
nerdkux <90501615+nerkux@users.noreply.github.com>
2025-07-20 13:57:18 +1000

    updated module structure

--- Maxon/modules/example_module.py
+++ Maxon/modules/example_module.py
@@ -1,5 +1,10 @@
 from Maxon.init import VERSION, START_TIME
 
+__name__ = "module_name"
+__description__ = "module_description"
+__version__ = "1.0"
+__author__ = "@username"
+
 async def handle(packet, args):
     return "example answer"
 
